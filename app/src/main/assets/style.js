4960712f7dd76b0cd04302e667c2b9c49083e9c4
piotr_madry <piotr.madry@appunite.com>
2017-09-13 23:38:58 +0200

    first commit

---
+++ app/src/main/assets/style.js
@@ -0,0 +1,5 @@
+function () {
+ document.getElementById("hplogo").src = "https://android.com.pl/images/user-images/2017/03/android-developer2.png";
+ document.getElementById("lst-ib").style.backgroundColor = "#FFFF3C";
+ document.getElementById("lst-ib").value = "Custom input!";
+}
---
+++ app/src/main/java/piotrmadry/demowebviewinjectedjs/MainActivity.kt
@@ -0,0 +1,45 @@
+package piotrmadry.demowebviewinjectedjs
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import java.io.BufferedReader
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById<WebView>(R.id.webView) as WebView
+
+        webView.settings.javaScriptEnabled = true
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                injectJS()
+            }
+        }
+        webView.loadUrl("https://www.google.com")
+    }
+
+    private fun injectJS() {
+        val jsContent: String?
+        jsContent = try {
+            val inputStream = assets.open("style.js")
+            val allText = inputStream.bufferedReader().use(BufferedReader::readText)
+            inputStream.close()
+            allText
+        } catch (e: Exception) {
+            null
+        }
+        jsContent?.let { webView.loadUrl("javascript:($jsContent)()") }
+    }
+}
